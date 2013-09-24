8083e9de7118272ad68eb8bdddf2671c4b257114
Molon <Molon@MolontekiMacBook-Pro.local>
2013-09-25 04:47:09 +0800

    Initial Commit

---
+++ MLNavigation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MLNavigation
+//
+//  Created by Molon on 13-9-25.
+//  Copyright (c) 2013年 Molon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
